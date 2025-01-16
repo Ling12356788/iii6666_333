7207b4334903dfcdd6e54c5f72b943bce4773221
Ling12356788 <sansan5678888@gmail.com>
2025-01-16 14:44:24 +0800

    Add files via upload

---
+++ Service Worker.sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'calculator-v1';
+const urlsToCache = [
+    '/iii6666_333/',
+    '/iii6666_333/index.html',
+    '/iii6666_333/manifest.json',
+    '/iii6666_333/icons/mm1.png',
+    '/iii6666_333/icons/mm2.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
